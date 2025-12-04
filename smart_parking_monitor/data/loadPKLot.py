fabba6105825b3b29202db977fcdff12c7416b4f
Parkhb1106 <stingyrain@gmail.com>
2025-12-04 21:45:48 +0900

    add PKLot dataset

---
+++ smart_parking_monitor/data/loadPKLot.py
@@ -0,0 +1,8 @@
+import os
+from pathlib import Path
+import kagglehub
+
+os.environ["KAGGLEHUB_CACHE"] = str(Path.cwd())
+
+path = kagglehub.dataset_download("ammarnassanalhajali/pklot-dataset")
+print("Path:", path)
